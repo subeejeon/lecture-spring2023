14f241f59af1b905041718fd4909a050c80bb78c
nasubeeni120 <145312273+nasubeeni120@users.noreply.github.com>
2023-12-21 21:29:54 +0900

    Initial Commit

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,24 @@
+package hello.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + req);
+        System.out.println("resoponse = " + resp);
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+    }
+}
